96e91e2c85b95960ba9891d920c9a0241002c314
Devi Mandasari <devimandas14@gmail.com>
2021-01-16 08:23:23 +0700

    keychain add

---
+++ AVShowcase/APICallManager/LocalStorageService.swift
@@ -0,0 +1,68 @@
+//
+//  LocalStorageService.swift
+//  AVShowcase
+//
+//  Created by Devi Mandasari on 16/01/21.
+//
+
+import Foundation
+
+class LocalStorageService {
+    
+    static let shared = LocalStorageService()
+    
+    // USER DEFAULT
+    let defaults = UserDefaults.standard
+    
+    // get interval on local storage
+    func getInterval() -> String {
+        
+        guard let interval = defaults.string(forKey: "interval") else {
+            return "15min"
+        }
+        
+        return interval
+    }
+    
+    // set interval
+    func setInterval(interval: String) {
+        defaults.set(interval, forKey: "interval")
+        defaults.synchronize()
+    }
+    
+    func cleanInterval(){
+        defaults.set(nil, forKey: "interval")
+        defaults.synchronize()
+    }
+    
+    func getOutputSize() -> String {
+        
+        guard let outputsize = defaults.string(forKey: "outputsize") else {
+            return "compact"
+        }
+        
+        return outputsize
+    }
+    
+    // set outputsize
+    func setOutputSize(outputsize: String) {
+        defaults.set(outputsize, forKey: "outputsize")
+        defaults.synchronize()
+    }
+    
+    func cleanOutputSize(){
+        defaults.set(nil, forKey: "outputsize")
+        defaults.synchronize()
+    }
+    
+    func setApikey(withApiKey apikey: String, withKey key: String) {
+        KeychainWrapper.standard.set(apikey, forKey: key)
+    }
+    
+    func getApikey() -> String {
+        let apikey = KeychainWrapper.standard.string(forKey: "apikey")
+        
+        return apikey ?? ""
+    }
+}
+
---
+++ AVShowcase/APICallManager/StockProvider.swift
@@ -0,0 +1,116 @@
+//
+//  StockProvider.swift
+//  AVShowcase
+//
+//  Created by Devi Mandasari on 16/01/21.
+//
+
+import Foundation
+
+public class StockProvider: StockService {
+    
+    public static let shared = StockProvider()
+    private init() {}
+    
+    // get api key and base url from user defined setting
+    private let baseUrl = "https://www.alphavantage.co"
+//    private let apiKey = "61F1SVWHXJH7NI2E"
+    private let apiKey = LocalStorageService.shared.getApikey()
+    
+    private let urlSession = URLSession.shared
+    
+    private let jsonDecoder: JSONDecoder = {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-mm-dd"
+        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return jsonDecoder
+    }()
+    
+    
+//    // MARK: get daily stock
+//    func getDailyStock(endpoint: StockEndpoint, params: [String : String]?, onSuccess: @escaping (DailyResponse) -> Void, onError: @escaping (Error) -> Void) {
+//        self.handeRequest(requestedObjectType: DailyResponse.self, endpoint: "/\(endpoint.rawValue)", params: params, onSuccess: onSuccess, onError: onError)
+//    }
+//
+//    // MARK: get intra-day stock
+//    func getIntraDayStock(endpoint: StockEndpoint, params: [String : String]?, onSuccess: @escaping (IntraDayResponse) -> Void, onError: @escaping (Error) -> Void) {
+//        self.handeRequest(requestedObjectType: IntraDayResponse.self, endpoint: "/\(endpoint.rawValue)", params: params, onSuccess: onSuccess, onError: onError)
+//    }
+    
+    // function for handling api Request with generic type, so it can always be used later
+    private func handeRequest<T:Codable>(requestedObjectType:T.Type, endpoint: String, params: [String : String]?, onSuccess: @escaping (T) -> Void, onError: @escaping (Error) -> Void){
+        
+        guard var urlComponents = URLComponents(string: "\(self.baseUrl)\(endpoint)") else {
+            onError(StockError.invalidEndpoint)
+            return
+        }
+        
+        var queryItems = [URLQueryItem(name: "apikey", value: apiKey)]
+        if let params = params {
+            queryItems.append(contentsOf: params.map { URLQueryItem(name: $0.key, value: $0.value) })
+        }
+        
+        urlComponents.queryItems = queryItems
+        
+        guard let url = urlComponents.url else {
+            onError(StockError.invalidEndpoint)
+            return
+        }
+        
+        print(url)
+        
+        urlSession.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                self.errorHandler(onErrorCallback: onError, error: StockError.errorFromApi)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
+                self.errorHandler(onErrorCallback: onError, error: StockError.invalidResponse)
+                return
+            }
+            
+            guard let data = data else {
+                self.errorHandler(onErrorCallback: onError, error: StockError.noData)
+                return
+            }
+            
+            do {
+                
+                let response = try self.jsonDecoder.decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    onSuccess(response)
+                }
+            } catch let DecodingError.dataCorrupted(context) {
+                self.errorHandler(onErrorCallback: onError, error: StockError.invalidResponse)
+                print(context)
+            } catch let DecodingError.keyNotFound(key, context) {
+                self.errorHandler(onErrorCallback: onError, error: StockError.invalidResponse)
+                print("Key '\(key)' not found:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch let DecodingError.valueNotFound(value, context) {
+                self.errorHandler(onErrorCallback: onError, error: StockError.invalidResponse)
+                print("Value '\(value)' not found:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch let DecodingError.typeMismatch(type, context)  {
+                self.errorHandler(onErrorCallback: onError, error: StockError.invalidResponse)
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {
+                self.errorHandler(onErrorCallback: onError, error: StockError.invalidResponse)
+                print("error: ", error)
+            }
+        }.resume()
+    }
+    
+    // handle error here
+    private func errorHandler(onErrorCallback: @escaping(_ error: Error) -> Void, error: Error) {
+        DispatchQueue.main.async {
+            onErrorCallback(error)
+        }
+    }
+    
+}
+
